2695e502a95f35af2451e69b76752282f4aca96b
GALI VENKATA GOPI <venkatagopigali6@gmail.com>
2026-04-19 22:08:37 +0530

    Ok

---
+++ python/day7.py
@@ -0,0 +1,38 @@
+# a="balakrishna"
+# print(a[0:4:1])
+# print(a[4:9:1])
+
+# print(a[-8:-5:1])
+
+# print(a[4:11:1])
+# print(a[4::])
+# print(a[1:11:2])
+# print(a[1::2])
+
+
+
+
+
+# a="balakrishna"
+# print(a[-11::2])
+# print(a[-10::3])
+# print(a[4:-4])
+# print(a[-9:9:2])
+# print(a[8:1:-2])
+# print(a[6:3:-1])
+# print(a[7:2:-2])
+
+# a="balakrishna"
+# print(a[9:-9:-2])
+# print(a[-3::-4])
+# print(a[::-1])
+
+
+name='gapi'
+name[1]='o'
+print(name)
+
+
+# name='gapi'
+# a=name.replace('a','o')
+# print(a)
