df7b7dc58fe1d5bc4307a509a509786c58c887fe
GALI VENKATA GOPI <venkatagopigali6@gmail.com>
2026-04-21 07:48:35 +0530

    ok

---
+++ python/day8.py
@@ -0,0 +1,41 @@
+# a="  Rahul  "
+# print(a.upper())
+# print(a)
+# print(a.lower())
+# print(len(a))
+
+# a="rahul"
+# print(a.replace('a','@'))
+# print(a.replace('rahul','prasanna'))
+# print(a.replace('rahul','gopi'))
+
+
+# a="    prasanna      "
+# print(a)
+# print(len(a))
+# print(a.strip())
+# print(len(a.lstrip()))
+# print(len(a.rstrip()))
+
+# a="hello "
+# print(a.startswith(' '))
+# print(a.endswith(' '))
+
+# a="PyTHon"
+# print(a.swapcase())
+
+# a='hello python welcome'
+# print(a.title())
+# print(a.capitalize())
+
+# a="my name is gopi"
+# print(a.split(' '))
+
+# a='i am from hyd'
+# print(a.split('m'))
+
+# a="gopi,b.tech,dataanalyst,trainer"
+# print(a.split(','))
+
+a=['my', 'name', 'is', 'gopi']
+print(','.join(a))
