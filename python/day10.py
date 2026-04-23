334a40dac68041d69dae4d1c4416672ffc57b92c
GALI VENKATA GOPI <venkatagopigali6@gmail.com>
2026-04-23 22:07:08 +0530

    ok

---
+++ python/day10.py
@@ -0,0 +1,66 @@
+# if 10!=10:
+#     print("both values are equal")
+
+
+# a='sai'
+# b='prasanna'
+# if a!=b:
+#     print("both are not same name")
+
+# a=30
+# b=40
+# if a>b:
+#     print('a is big')
+
+
+# if 10!=10:
+#     print("both are same")
+# else:
+#     print("both are not same")
+
+
+# status='not completed'
+# if status=='completed':
+#     print("hike")
+# else:
+#     print("not get a hike")
+
+
+# a=50
+# b=40
+# if a>b:
+#     print('a is big')
+# else:
+#     print('b is big')
+
+# a=40
+# b=40
+# if a>b:
+#     print('a is big')
+# elif a==b:
+#     print("both are equal")
+# else:
+#     print("b is big")
+
+
+# season='winter'
+# if season=='summer':
+#     print("it's very hot")
+# elif season=='rain':
+#     print("it's rainy season")
+# else:
+#     print('very cool')
+
+marks=36
+if marks>90:
+    print("A grade")
+elif marks>80:
+    print("B grade")
+elif marks>70:
+    print("C grade")
+elif marks>60:
+    print("D grade")
+elif marks>35:
+    print('E grade')
+else:
+    print("Fail")
---
+++ python/day9.py
@@ -0,0 +1,40 @@
+# a="prasanna123"
+# print(type(a))
+# print(a.isupper())
+# print(a.islower())
+# print(a.isalpha())
+
+# a='12345a'
+# print(type(a))
+# print(a.isdigit())
+
+# a='123'
+# print(a.isalnum())
+
+
+# print(10+20)
+# print('10'+'20')
+# print('sai'+' '+'prasanna')
+
+
+# a=10
+# print(10*5)
+# print('10'*5)
+# print('rahul'*10)
+
+
+# print('o' in 'gopi')
+# print('op' in 'gopi')
+# print('oi' in 'gopi')
+# print('O' in 'gopi')
+
+# print('a' not in 'gopi')
+# print('oi' not in 'gopi')
+
+# print(ord('a'))
+# print(ord('A'))
+# print(chr(108))
+
+print('prasanna'.count('a'))
+print('prasanna'.count('n'))
+print('PRASANNA'.count('AN'))
