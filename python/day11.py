0cef6ef53b0b01bf25a292ca0a43c24f9eec7252
GALI VENKATA GOPI <venkatagopigali6@gmail.com>
2026-04-24 22:50:01 +0530

    ok

---
+++ python/day11.py
@@ -0,0 +1,64 @@
+# open instagram
+
+# s_lock=123
+# i_lock=4567
+# if s_lock==1234:
+#     print("your phone is opened")
+#     if i_lock==4567:
+#         print("insta is open")
+#     else:
+#         print('insta not open')
+# else:
+#     print('phone not open')
+
+
+# a=99
+# if a>100:
+#     if a>200:
+#         print('more then 200')
+#     else:
+#         print('lessthen or same to 200 ')
+# else:
+#     print('less then 100 or same')
+
+# a=5
+# if a==1:
+#     print("one")
+# elif a==2:
+#     print('two')
+# elif a==3:
+#     print("three")
+# else:
+#    print('not in 1,2,3')
+
+# a=11
+# match a:
+#     case 11:
+#         print("one")
+#     case 22:
+#         print("two")
+#     case 30:
+#         print("three")
+#     case _:
+#         print('not found')
+
+# a=-1
+# if a<0:
+#     print('negative number')
+# elif a>0:
+#     print("positive number")
+# else:
+#     print("zero")
+
+# a=9
+# if a%2==0:
+#     print("even")
+# else:
+#     print("odd")
+
+
+age=17
+if age>=18:
+    print("eligible to vote")
+else:
+    print("not eligible")
