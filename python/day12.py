44b390b460fea4c62e9e791b3d03180223e7938c
GALI VENKATA GOPI <venkatagopigali6@gmail.com>
2026-04-26 23:09:33 +0530

    ok

---
+++ python/day12.py
@@ -0,0 +1,34 @@
+# a=1
+# if a%2==0:
+#     print("even")
+# else:
+#     print("odd")
+# a=2
+# if a%2==0:
+#     print("even")
+# else:
+#     print("odd")
+# a=3
+# if a%2==0:
+#     print("even")
+# else:
+#     print("odd")
+
+
+# for i in range(1,10,1):
+#     if i%2==0:
+#         print(i,'-is even')
+#     else:
+#         print(i,'-not even')
+
+
+# for i in range(1,51):
+#     # print(i)
+#     if i%5==0:
+#         print(i)
+
+c=0
+for i in range(1,10):
+    if i%2==0:
+        c=c+1   # c+=1
+print(c)
