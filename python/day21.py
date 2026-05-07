137e6f7025eceaa84eb2e25bc32d905a3972db9f
GALI VENKATA GOPI <venkatagopigali6@gmail.com>
2026-05-07 22:01:35 +0530

    ok

---
+++ python/day21.py
@@ -0,0 +1,37 @@
+# def even(a):
+#     if a%2==0:
+#         print(a,"even")
+#     else:
+#         print(a,'odd')
+
+# even(10)
+# even(50)
+# even(49)
+
+# def Max_value(l):
+#     m=l[0]
+#     for i in l:
+#         if i>m:
+#             m=i
+#     print(m)
+
+# a=[10,20,30,40,0,9,55,9,60.5]
+# Max_value(a)
+# b=[33,44,22,77,66]
+# Max_value(b)
+# c=(10,40,66,88,9,109,110)
+# Max_value(c)
+
+
+# a=[1,2,3,4,5,5,6]
+# print(a.count(5))
+
+# def add(a,b):
+#     print(a+b)
+# add(10,20)
+
+
+
+def greet():
+    print("hello ,how are you")
+greet()
