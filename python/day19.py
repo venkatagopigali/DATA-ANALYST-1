ad52c25e99ce0e24749b32dcfb42f9088af634f6
GALI VENKATA GOPI <venkatagopigali6@gmail.com>
2026-05-07 15:02:20 +0530

    ok

---
+++ python/day19.py
@@ -0,0 +1,35 @@
+# s={10,10,20,'a','python',10.5,'python'}
+# print(s[0])
+
+# a={10,20,30,40}
+# a.add(30)
+# print(a)
+
+# a={10,20,30,40,50}
+# a.remove(60)
+# print(a)
+
+# a={10,20,30,40,50}
+# a.discard(60)
+# print(a)
+
+# a={10,20,30,40,50,'a'}
+# a.pop()
+# print(a)
+
+
+# a={10,20,30,40,50,'a'}
+# a.clear()
+# print(a)
+
+s1={1,2,3,4,5}
+s2={3,4,5,6,7}
+# print(s1|s2)
+# print(s1.union(s2))
+# print(s1&s2)
+# print(s1.intersection(s2))
+# print(s1-s2)
+# print(s1.difference(s2))
+# print(s2-s1)
+print(s1^s2)
+print(s1.symmetric_difference(s2))
---
+++ python/day20.py
@@ -0,0 +1,34 @@
+# d={"name":"ajay","age":28,"qualification":'Msc'}
+# d['name']='gopi'
+# print(d)
+
+
+
+# d={"name":"ajay","age":28,"qualification":'Msc'}
+# d['name2']='ajay'
+# d['new_age']=28
+# d['name']='ajay'
+# print(d)
+
+
+# d={"name":"ajay","age":28,"qualification":'Msc'}
+# print(d["qualification"])
+
+# d={'name':'gopi','position':'data scientist','qualifiaction':'Msc'}
+# print(d.keys())
+# print(d.values())
+# print(d.items())
+# d.popitem()
+# d.pop('position')
+# print(d)
+# print(d['name'])
+# print(d.get('name'))
+
+
+
+d={'name':'gopi','position':'data scientist','qualification':'Msc'}
+# d['name']='raju'
+# d.update({'name':'raju','qualification':'M.Tech'})
+# print(d)
+d.clear()
+print(d)
