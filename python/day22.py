bb0f5c4923389a53344e2db64d67b7c3bf9aef72
GALI VENKATA GOPI <venkatagopigali6@gmail.com>
2026-05-09 22:03:03 +0530

    ok

---
+++ python/day22.py
@@ -0,0 +1,115 @@
+# # def first_function():
+# #     print("my function")
+# #     print("create a logic")
+
+
+
+
+# # calling the function
+# # first_function()
+
+# def add():
+#     a=10
+#     b=20
+#     print(a+b)
+
+# def sub():
+#     a=10
+#     b=20
+#     print(a-b)
+
+# def mul():
+#     a=100
+#     b=200
+#     print(a*b)
+
+# print("1.addition\n2.sub\n3.mut")
+# op=int(input("eneter a number"))
+# if op==1:
+#     add()
+# elif op==2:
+#     sub()
+# elif op==3:
+#     mul()
+
+
+# add()
+# sub()
+# mul()
+
+
+# def add(a,b):
+#     print(a+b)
+
+# add(10,20)
+
+
+
+#without parameter
+# def calculation():
+#     a=10
+#     b=20
+#     print(a+b)
+#     print(a-b)
+#     print(a*b)
+#     print(a/b)
+
+# calculation()
+
+#with parameters
+# def calculation(a,b):
+#     print(a+b)
+#     print(a-b)
+#     print(a*b)
+#     print(a/b)
+
+# calculation(101,201)
+
+
+
+# default arguments
+
+# def cal(a=10,b=20):
+#     print(a+b)
+#     print(a-b)
+#     print(a*b)
+
+# cal()
+
+# positional
+
+
+# def cal(a,b,c):
+#     print(a)
+#     print(b)
+#     print(c)
+#     print(a+b)
+#     print(a-b)
+
+# cal(20,10,100)
+
+# def add(a,b=30):
+#     print(a+b)
+
+# add(10)
+# add(10,50)
+
+#keyword arguments
+# def add(a,b,c):
+#     print(a)
+#     print(b)
+#     print(c)
+
+# add(c=30,b=20,a=10)
+
+
+# def cal(a,b,c,d):
+#     print(a)
+#     print(b)
+#     print(c)
+#     print(d)
+# cal(10,20,d=30,c=40)
+
+def cal(*a):
+    print(sum(a))
+cal(10,20,30,40,50)
