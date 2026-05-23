5bdf2329bb66e5699d6854864937eae18a7af011
GALI VENKATA GOPI <venkatagopigali6@gmail.com>
2026-05-23 22:54:40 +0530

    ok

---
+++ python/day33.py
@@ -0,0 +1,21 @@
+# from abc import ABC,abstractmethod
+# class Bank(ABC):
+#     @abstractmethod
+#     def withdraw(self):
+#         amount=1000
+#         a=int(input("enter amount   : "))
+#         if amount<a:
+#             print("insuffient balence")
+#         else:
+#             amount=amount-a
+#             print("balence amount   ",amount)
+# class atm(Bank):
+#     def withdraw(self):
+#       super().withdraw()
+
+
+# a=atm()
+# a.withdraw()
+
+# b=Bank()
+# b.withdraw()
